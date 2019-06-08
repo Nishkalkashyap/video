941da29ee141174497af28b39580edf245d16f6e
Nishkal <kashyapnishkal@gmail.com>
2019-06-08 18:33:07 +0530

    removed failing video

--- src/videos.ts
+++ src/videos.ts
@@ -23,18 +23,18 @@ export const videos: Video[] = [
         // ],
         mp3: './mp3/bensound-betterdays.mp3'
     },
-    {
-        output: './out/creativeminds.mp4',
-        videos: [
-            'https://pixabay.com/videos/download/video-6516_large.mp4',
-            'https://pixabay.com/videos/download/video-7826_source.mp4',
-            'https://pixabay.com/videos/download/video-21147_source.mp4',
-            'https://pixabay.com/videos/download/video-3681_source.mp4',
-            'https://pixabay.com/videos/download/video-6515_large.mp4',
-            'https://pixabay.com/videos/download/video-22601_source.mp4',
-        ],
-        mp3: './mp3/bensound-creativeminds.mp3'
-    },
+    // {
+    //     output: './out/creativeminds.mp4',
+    //     videos: [
+    //         'https://pixabay.com/videos/download/video-6516_large.mp4',
+    //         'https://pixabay.com/videos/download/video-7826_source.mp4',
+    //         'https://pixabay.com/videos/download/video-21147_source.mp4',
+    //         'https://pixabay.com/videos/download/video-3681_source.mp4',
+    //         'https://pixabay.com/videos/download/video-6515_large.mp4',
+    //         'https://pixabay.com/videos/download/video-22601_source.mp4',
+    //     ],
+    //     mp3: './mp3/bensound-creativeminds.mp3'
+    // },
     {
         output: './out/creativeminds.mp4',
         videos: [
